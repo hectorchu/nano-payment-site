1bf34081951d870df2c8926265fe553267c9f038
Hector Chu <hectorchu@gmail.com>
2021-02-06 20:27:10 +0100

    Add Nano Payment

--- src/App.js
+++ src/App.js
@@ -1,24 +1,96 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import {Alert, Button, Col, Container, Form, Row} from 'react-bootstrap';
+import NanoPayment from './NanoPayment';
+
+let controller = new AbortController();
+
+function postData(url, data) {
+  return fetch(url, {
+    method: 'POST',
+    headers: {'Content-Type': 'application/json'},
+    body: JSON.stringify(data),
+    signal: controller.signal,
+  }).then(resp => {
+    if (resp.status != 200) {
+      return resp.text().then(data => {
+        throw new Error(data);
+      });
+    }
+    return resp.json();
+  });
+}
 
 function App() {
+  const [account, setAccount] = useState("");
+  const [account2, setAccount2] = useState("");
+  const [amount, setAmount] = useState("");
+  const [show, setShow] = useState(false);
+  const [blockHash, setBlockHash] = useState("");
+  const [error, setError] = useState();
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    controller = new AbortController();
+    postData('https://gonano.dev/payment/new', {account, amount})
+    .then(data => {
+      setAccount2(data.account);
+      setShow(true);
+      return postData('https://gonano.dev/payment/wait', {id: data.id, timeout: "1800"});
+    })
+    .then(data => {
+      setShow(false);
+      setBlockHash(data['block_hash']);
+    })
+    .catch(err => {
+      if (err.name != 'AbortError') setError(err);
+    });
+  }
+
+  const handleClose = () => {
+    setShow(false);
+    controller.abort();
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <h1>Gonano Payments</h1>
+      <div style={{marginTop: 20}} />
+
+      {blockHash &&
+        <Alert variant="success" onClose={() => setBlockHash("")} dismissible>
+          Received with block hash{' '}
+          <Alert.Link target="_blank" href={"https://nanolooker.com/block/"+blockHash}>{blockHash}</Alert.Link>
+        </Alert>
+      }
+
+      {error &&
+        <Alert variant="danger" onClose={() => setError()} dismissible>
+          Error: {error.message}
+        </Alert>
+      }
+
+      <Form onSubmit={handleSubmit}>
+        <Form.Group as={Form.Row}>
+          <Form.Label column sm="2">Account to receive on</Form.Label>
+          <Col sm="7">
+            <Form.Control type="text" placeholder="Account" value={account} onChange={e => setAccount(e.target.value)} />
+          </Col>
+        </Form.Group>
+        <Form.Group as={Form.Row}>
+          <Form.Label column sm="2">Amount to receive</Form.Label>
+          <Col sm="7">
+            <Form.Control type="number" placeholder="Amount" value={amount} onChange={e => setAmount(e.target.value)} />
+          </Col>
+          <Form.Label column sm="2">NANO</Form.Label>
+        </Form.Group>
+        <Button variant="primary" type="submit" disabled={!account || amount <= 0}>Submit payment request</Button>
+      </Form>
+
+      <NanoPayment
+        account={account2} amount={amount}
+        show={show} onClose={handleClose}
+      />
+    </Container>
   );
 }
 
---
+++ src/NanoPayment.js
@@ -0,0 +1,27 @@
+import React, { useState } from 'react';
+import {Button, Modal} from 'react-bootstrap';
+import QRCode from 'qrcode.react';
+import Big from 'big.js';
+
+export default function NanoPayment({
+  account, amount, show, onClose,
+}) {
+  return (
+    <Modal centered show={show} onHide={onClose}>
+      <Modal.Header closeButton>
+        <Modal.Title>NANO Payment Request</Modal.Title>
+      </Modal.Header>
+
+      <Modal.Body>
+        <p>Please send {amount} NANO to <pre>{account}</pre></p>
+        <div style={{textAlign: 'center'}}>
+          <QRCode value={`nano:${account}?amount=${Big(amount||0).times(1e30).toFixed(0)}`} />
+        </div>
+      </Modal.Body>
+
+      <Modal.Footer>
+        <Button variant="secondary" onClick={onClose}>Cancel</Button>
+      </Modal.Footer>
+    </Modal>
+  )
+}
